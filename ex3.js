849904d5698293c8fabe16872260cf76a6bbb1c2
mdhake1-dbs <20078257@mydbs.ie>
2025-10-06 12:59:10 +0100

    Final Exercide 3

--- ex3.js
+++ ex3.js
@@ -1,4 +1,4 @@
-let euler1 = (str1, str2, str3) => {
+let eulerList1 = (str1, str2, str3) => {
 
     let m1 = intinp(str1);
     let m2 = intinp(str2);
@@ -13,5 +13,32 @@ let euler1 = (str1, str2, str3) => {
             sum += myar1[i];
         }
     }
+    alert("Sum of Multiples = " + sum);
+}
+
+let eulerList2 = (str1, str2) => {
+
+    let divarr = intarrinp(str1);
+    let numlist = intarrinp(str2);
+
+    let sum = 0;
+
+    //Logic - 1
+    for (let i = 0; i < numlist.length; i++) {
+        if (divarr.some(d => numlist[i] % d === 0)) {
+            sum += numlist[i];
+        }
+    }
+
+    let divisors = [m1, m2, m3];
+
+    //Logic - 2
+    /*
+    sum = myar1.reduce((acc, val) => {
+        return divisors.some(d => val % d === 0) ? acc + val : acc;
+    }, 0);
+    */
+
+
     alert("Sum of Multiples = " + sum);
 }
